3faf88241bbd0f5b3249f27dabe0d9cc06d8ae2a
pcanella <pcanella@ebay.com>
2015-12-04 16:40:14 -0800

    More and more updates

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
- /* global document, window */
+ /* global document, window, CustomEvent */
  'use strict';
 
- var vcr = function(url, element, options) {
-     this.set(url, element, options);
+ var vcr = function(config, options) {
+     this.set(config, options);
  };
 
  vcr.prototype = {
@@ -23,7 +23,7 @@
 
      setVideoType: function() {
          if (this.parsedData !== undefined) {
-             this.videoInstance = (this.parsedData.type === 'vimeo') ? new this.vimeo : (this.parsedData.type === 'youtube') ? new this.youtube : '';
+             this.videoInstance = (this.parsedData.type === 'vimeo') ? new this.vimeo() : (this.parsedData.type === 'youtube') ? new this.youtube() : '';
          }
 
          return this.videoInstance;
@@ -31,38 +31,88 @@
 
      player: null,
 
-     set: function(url, element, options) {
+     setListeners: function() {
+
+     },
+
+     set: function(config, options) {
+         var url, element;
+
+         // if it's an object, then we know it doesn't have 
+         if (config instanceof Object) {
+             // If an element is given, then keep it, otherwise make up a vcr name and create the element!
+             element = (config.el) ? config.el : 'vcrPlayer' + Math.floor(Math.random() * 6000) + 1;
+             url = (config.url) ? config.url : '';
+             if (element && config.url && document.querySelector('#' + config.el) === null) {
+                 this.whereToPut(element, config);
+             } else if (element && !config.url) {
+                 element = config.el;
+                 url = document.getElementById(element).src;
+             } else {
+                 console.error('Hmm, I don\'t think you added an element id or url...');
+             }
+             this.el = element;
+
+             // If the user inputted a URL, that means we want to create an iframe, otherwise, config is just an element ID that we can use
+         } else {
+             element = config;
+             url = document.getElementById(element).src;
+             this.el = element;
+         }
 
          var self = this,
              playerParams = (options && options.parameters) ? options.parameters : {};
+
          // First we'll parse the data properly, then see if it's a youtube/ vimeo/html5 video. 
          this.parsedData = this.parser.parse(url);
-         this.videoType = this.setVideoType();
 
-         this.el = element;
-         this.player = document.getElementById(this.el);
+         this.videoType = this.setVideoType();
 
          var type = this.videoType,
              id = this.parsedData.id;
 
-         if (options.autoplay) {
+         if (options && options.autoplay) {
              playerParams.autoplay = 1;
          }
          if (type !== '') {
+
+             this.player = document.getElementById(this.el);
+             // Initial viewport check
+
+             this.player.addEventListener('vcr:ready', function() {
+                 console.log('video ready!');
+                 if (options && options.scrollStop) {
+                     self.throttleVideo();
+                     var throttler = self.debounce(self.throttleVideo, 250);
+                     window.addEventListener('scroll', throttler);
+                 }
+             });
              playerParams = this.setOptions(playerParams);
-             console.log(playerParams);
+
              this.videoType.setPlayer(id, element, playerParams);
          }
-         if (options.scrollStop) {
-             // Initial viewport check
-             this.player.addEventListener('vcr:videoReady', function() {
-                 self.throttleVideo();
-             });
-             // Viewport check on scrolling
-             var throttler = this.debounce(this.throttleVideo, 250);
-             window.addEventListener('scroll', throttler);
-         }
+     },
 
+     whereToPut: function(el, config) {
+
+         var c = config,
+             // Create a new iframe!
+             frame = document.createElement('iframe');
+         
+         frame.id = el;
+         frame = frame.outerHTML;
+         // Probably could be refactored somehow. Suggestions welcome!
+         if (c.insertBefore) {
+             document.querySelector(c.insertBefore).insertAdjacentHTML('beforebegin', frame);
+         } else if (c.insertAfter) {
+             document.querySelector(c.insertAfter).insertAdjacentHTML('afterEnd', frame);
+         } else if (c.appendTo) {
+             document.querySelector(c.appendTo).insertAdjacentHTML('beforeend', frame);
+         } else if (c.prependTo) {
+             document.querySelector(c.prependTo).insertAdjacentHTML('afterbegin', frame);
+         } else {
+             document.body.insertAdjacentHTML('afterbegin', frame);
+         }
      },
 
      setOptions: function(params) {
@@ -71,6 +121,8 @@
      },
 
      play: function() {
+         var e = new CustomEvent('vcr:play');
+         document.getElementById(this.el).dispatchEvent(e);
          this.videoType.play();
      },
 
@@ -106,7 +158,6 @@
      // MIGHT PUT IN SEPARATE UTIL FOLDER
 
      throttleVideo: function() {
-         debugger;
          if (this.withinViewport(this.el) === false) {
              this.pause();
          } else {
@@ -116,8 +167,6 @@
      },
 
      debounce: function(func, wait, immediate) {
-         debugger;
-
          var timeout, context = this;
          return function() {
              var args = arguments;
@@ -136,12 +185,14 @@
          };
      },
 
+     // TODO: FIX THIS FOR SMALLER SCREENS
      withinViewport: function(el) {
-         var element = document.getElementById(el);
-         //special bonus for those using jQuery
-         var rect = element.getBoundingClientRect();
-         debugger;
-         return (rect.top <=  window.innerHeight/2 /*|| rect.bottom >= 100*/ );
+         var element = document.getElementById(el),
+             rect = element.getBoundingClientRect(),
+             oneThird = element.offsetHeight / 3,
+             leftover = window.innerHeight - rect.top;
+         console.log('el:', el, 'top:', rect.top, 'bottom:', rect.bottom);
+         return (rect.top > 0 && leftover > (element.offsetHeight / 2) || (rect.top < (element.offsetHeight - oneThird) && rect.bottom >= oneThird));
      }
 
      // TODO: Implement this at some other time
